607029cbb59d3f6a657ac0cb06d8760954948ccc
ArtOfCode- <hello@artofcode.co.uk>
2017-08-21 16:29:07 +0100

    Another seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 OffenseType.create([{ name: 'Suspected Sockpuppetry' }, { name: 'Voting Fraud' }])
-ReviewResultType.create([{ name: 'Suspended' }, { name: 'Voting Reversed' }, { name: 'No Action Needed' }])
+ReviewResultType.create([{ name: 'Suspended' }, { name: 'Voting Reversed' }, { name: 'No Action Needed' }, { name: 'Flagged for Moderator' }])
